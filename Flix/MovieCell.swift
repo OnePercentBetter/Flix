7d2328bba60410f997c71a4eb17b58f5ef7f8d55
OnePercentBetter <98249853+OnePercentBetter@users.noreply.github.com>
2022-02-13 19:28:53 -0500

    Flix App

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Sal on 2/13/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var posterLabel: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
